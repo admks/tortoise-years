d4ed87dc2a274a75eb311186fc86cadeba4273c7
mirza <alexbrost7@gmail.com>
2021-12-12 16:00:17 +0300

    convert human years to tortoise years

---
+++ script1.js
@@ -0,0 +1,9 @@
+let myAge = prompt (`Enter your age:`); // enter age 
+
+firstAge=12; // The first year of a tortoise’s life
+let laterYears = myAge - 1; // Total of remaining years 
+laterYears = laterYears*10; //Each year following equates to 10 human years.
+let tortoiseYears= firstAge+laterYears;//total of tortoise years
+let myName= "mirza";    //my name
+console.log(`Benim adım ${myName}. Kaplumbağa yılı olarak ${tortoiseYears} yaşındayım, insan yılı olarak ${myAge} yaşındayım.`); // Output sentence)
+alert(`Benim adım ${myName}. Kaplumbağa yılı olarak ${tortoiseYears} yaşındayım, insan yılı olarak ${myAge} yaşındayım.`); 
